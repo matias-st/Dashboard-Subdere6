c8a149c123a0dc23ede4de6c9ffb38a0e7cb1a26
matias-st <matias.odb@hotmail.com>
2020-12-01 21:07:14 -0300

    Version casi final 1
    Se integrarón y se ordenarón todos los avances de la aplicación

--- server.R
+++ server.R
@@ -1,54 +1,48 @@
-
-
 #shinyServer() sirve para desarrollar todas las funciones de la aplicacion.
 shinyServer(function(input, output) {
-    
-    ##se crean las variables BDiniciativas y BDregiones, luego con read_sheet() se lee el excel desde google drive con el link, sheet es igual a la hoja que se esta cargando.
-    BDiniciativas0 <- read_sheet("https://docs.google.com/spreadsheets/d/1QkMjIkeZgyCdhZYTHwZai9BsjN2lamvf_8AgwSRS5XI/edit#gid=0%22",
-                                sheet = "iniciativas")
-    BDregiones <- read_sheet("https://docs.google.com/spreadsheets/d/1QkMjIkeZgyCdhZYTHwZai9BsjN2lamvf_8AgwSRS5XI/edit#gid=0", 
-                             sheet="poblacion potencial")
-    BDseguimiento <- read_sheet("https://docs.google.com/spreadsheets/d/1QkMjIkeZgyCdhZYTHwZai9BsjN2lamvf_8AgwSRS5XI/edit#gid=1120700756",
-                                sheet = "seguimiento")
-    
   
-
-    
-##output$ind... envia al ui.R las cajas de todos de los indicadores que se ven en infobox.
-    output$indIniciativasAtrasadasReg <- renderInfoBox({
-        infoBox(
-            "Iniciativas atrasadas", "12%", icon = icon("fas fa-calendar-times"),
-            width = 6, color = "red", fill = TRUE
-        )
-    })
-    output$indIniciativasAtrasadasNac <- renderInfoBox({
-        infoBox(
-            "Iniciativas atrasadas", "15%", icon = icon("fas fa-calendar-times"),
-            width = 6, color = "red", fill = TRUE
-        )
-    })
-    
-    
-    numeroIniciativasTotales <- nrow(BDiniciativas0)
+  ##Indicadores que se ven en forma de infobox.
+  output$indIniciativasAtrasadasReg <- renderInfoBox({
+    iniciativasAtrasadas <- filter(BDactividades, actAtrasada == "atrasado")
+    numeroIniciativasAtrasadas <- nrow(iniciativasAtrasadas)
+    indIniciativasAtrasadas <- (numeroIniciativasAtrasadas/numeroIniciativasTotales)*100
+    aproximacionInicAtras <- round(indIniciativasAtrasadas, 2)
+    porcentajeInicAtras <- str_c( aproximacionInicAtras, "%")
+    infoBox(
+      "Iniciativas atrasadas", porcentajeInicAtras, icon = icon("fas fa-calendar-times"),
+      width = 6, color = "red", fill = TRUE
+    )
+  })
+  output$indIniciativasAtrasadasNac <- renderInfoBox({
+    indIniciativasAtrasadasNacional <- mean(BDnacional$'Iniciativas atrasadas')
+    aproximacionInicAtrasNac <- round(indIniciativasAtrasadasNacional, 2)
+    porcentajeInicAtrasNac <- str_c( aproximacionInicAtrasNac, "%")
+    infoBox(
+      "Iniciativas atrasadas", porcentajeInicAtrasNac, icon = icon("fas fa-calendar-times"),
+      width = 6, color = "red", fill = TRUE
+    )
+  })
+  output$indSectorPriorizadoReg <- renderInfoBox({
     iniciativasSectorPriorizado <- filter(BDiniciativas0, Sector == "Turismo" | Sector == "Agroindustria" | Sector == "Energía" | Sector == "Agroindustria/Industria")
     numeroIniciativasSectPr <- nrow(iniciativasSectorPriorizado)
     indSecPriorizado <- (numeroIniciativasSectPr/numeroIniciativasTotales)*100
     aproximacionSecPriorizado <- round(indSecPriorizado, 2)
     porcentajeSectPrio <- str_c( aproximacionSecPriorizado, "%")
-    output$indSectorPriorizadoReg <- renderInfoBox({
-        infoBox(
-            "Iniciativas en sector priorizado", porcentajeSectPrio , icon = icon("fas fa-check-circle"),
-            width = 6, color = "green", fill = TRUE
-        )
-    })
-    
-    output$indSectorPriorizadoNac <- renderInfoBox({
-        infoBox(
-            "Iniciativas en sector priorizado", "76%", icon = icon("fas fa-check-circle"),
-            width = 6, color = "green", fill = TRUE
-        )
-    })
-    
+    infoBox(
+      "Iniciativas en sector priorizado", porcentajeSectPrio , icon = icon("fas fa-check-circle"),
+      width = 6, color = "green", fill = TRUE
+    )
+  })
+  output$indSectorPriorizadoNac <- renderInfoBox({
+    indSecPriorizadoNac <- mean(BDnacional$'Iniciativas en sector priorizado')
+    aproximacionSecPriorizadoNac <- round(indSecPriorizadoNac, 2)
+    porcentajeSectPrioNac <- str_c( aproximacionSecPriorizadoNac, "%")
+    infoBox(
+      "Iniciativas en sector priorizado", porcentajeSectPrioNac, icon = icon("fas fa-check-circle"),
+      width = 6, color = "green", fill = TRUE
+    )
+  })
+  output$indCrecimientoSPReg <- renderInfoBox({
     añoActual <- max(BDiniciativas0$Año)
     añoAnterior <- añoActual - 1
     iniciativasSectPrioAñoActual <- filter(BDiniciativas0, Año == añoActual & Sector == "Turismo" | Sector == "Agroindustria" | Sector == "Energía" | Sector == "Agroindustria/Industria")
@@ -57,130 +51,191 @@ shinyServer(function(input, output) {
     numeroIniciativasSectPrAñoAnterior <- nrow(iniciativasSectPrioAñoAnterior)
     totalIniciativasAñoAnterior <- filter(BDiniciativas0, Año == añoAnterior)
     numeroIniciativasAñoAnterior <- nrow(totalIniciativasAñoAnterior)
-    
     indCrecSecPriorizado <- ((numeroIniciativasSectPrAñoActual-numeroIniciativasSectPrAñoAnterior)/numeroIniciativasAñoAnterior)*100
     aproximacionCrecSecPriorizado <- round(indCrecSecPriorizado, 2)
     porcentajeCrecSectPrio <- str_c( aproximacionCrecSecPriorizado, "%")
-    output$indCrecimientoSPReg <- renderInfoBox({
-        infoBox(
-            "Crecimiento en sectores priorizados", porcentajeCrecSectPrio, icon = icon("fas fa-chart-line"),
-            width = 6, color = "blue", fill = TRUE
-        )
-    })
-    
-    output$indCrecimientoSPNac <- renderInfoBox({
-        infoBox(
-            "Crecimiento en sectores priorizados", "2%", icon = icon("fas fa-chart-line"),
-            width = 6, color = "blue", fill = TRUE
-        )
-    })
-    
-    output$indBeneficiariosReg <- renderInfoBox({
-        infoBox(
-            "Beneficiarios efectivos cubiertos", "80%", icon = icon("fas fa-users"),
-            width = 6, color = "yellow", fill = TRUE
-        )
-    })
-    output$indBeneficiariosNac <- renderInfoBox({
-        infoBox(
-            "Beneficiarios efectivos cubiertos", "68%", icon = icon("fas fa-users"),
-            width = 6, color = "yellow", fill = TRUE
-        )
-    })
-    
-    output$indCrecimientoBenefReg <- renderInfoBox({
-        infoBox(
-            "Crecimiento beneficiarios cubiertos", "3%", icon = icon("fas fa-chart-line"),
-            width = 6, color = "orange", fill = TRUE
-        )
-    })
-    output$indCrecimientoBenefNac <- renderInfoBox({
-        infoBox(
-            "Crecimiento beneficiarios cubiertos", "1%", icon = icon("fas fa-chart-line"),
-            width = 6, color = "orange", fill = TRUE
-        )
-    })
-    
-    ##con los siguientes 3 output$... se envian al ui.R los gráficos de barra.
+    infoBox(
+      "Crecimiento en sectores priorizados", porcentajeCrecSectPrio, icon = icon("fas fa-chart-line"),
+      width = 6, color = "blue", fill = TRUE
+    )
+  })
+  output$indCrecimientoSPNac <- renderInfoBox({
+    indCrecSecPriorizadoNac <- mean(BDnacional$'Crecimiento en sectores priorizados')
+    aproximacionCrecSecPriorizadoNac <- round(indCrecSecPriorizadoNac, 2)
+    porcentajeCrecSectPrioNac <- str_c( aproximacionCrecSecPriorizadoNac, "%")
+    infoBox(
+      "Crecimiento en sectores priorizados", porcentajeCrecSectPrioNac, icon = icon("fas fa-chart-line"),
+      width = 6, color = "blue", fill = TRUE
+    )
+  })
+  output$indBeneficiariosReg <- renderInfoBox({
+    sumaBenefObjetivos <- colSums (BDseguimiento[ , 6])
+    sumaBenefEfectivos <- colSums (BDseguimiento[ , 7])
+    indBenefEfectivos <- (sumaBenefEfectivos/sumaBenefObjetivos)*100
+    aproximacionBenefEfectivos <- round(indBenefEfectivos, 2)
+    porcentajeBenefEfectivos <- str_c( aproximacionBenefEfectivos, "%")
+    infoBox(
+      "Beneficiarios efectivos cubiertos", porcentajeBenefEfectivos, icon = icon("fas fa-users"),
+      width = 6, color = "yellow", fill = TRUE
+    )
+  })
+  output$indBeneficiariosNac <- renderInfoBox({
+    indBenefEfectivosNac <- mean(BDnacional$'beneficiarios efectivos cubiertos')
+    aproximacionBenefEfectivosNac <- round(indBenefEfectivosNac, 2)
+    porcentajeBenefEfectivosNac <- str_c( aproximacionBenefEfectivosNac, "%")
+    infoBox(
+      "Beneficiarios efectivos cubiertos", porcentajeBenefEfectivosNac, icon = icon("fas fa-users"),
+      width = 6, color = "yellow", fill = TRUE
+    )
+  })
+  output$indCrecimientoBenefReg <- renderInfoBox({
+    iniciativasAñoActual <- filter(BDseguimiento, Año == añoActual)
+    iniciativasAñoAnterior <- filter(BDseguimiento, Año == añoAnterior)
+    sumaBenefEfectivosAñoActual <- colSums (iniciativasAñoActual[ , 7])
+    sumaBenefEfectivosAñoAnterior <- colSums (iniciativasAñoAnterior[ , 7])
+    indCrecBeneficiarios <- ((sumaBenefEfectivosAñoActual-sumaBenefEfectivosAñoAnterior)/sumaBenefEfectivosAñoAnterior)*100
+    aproximacionCrecBeneficiarios <- round(indCrecBeneficiarios, 2)
+    porcentajeCrecBeneficiarios <- str_c( aproximacionCrecBeneficiarios, "%")
+    infoBox(
+      "Crecimiento beneficiarios cubiertos", porcentajeCrecBeneficiarios, icon = icon("fas fa-chart-line"),
+      width = 6, color = "orange", fill = TRUE
+    )
+  })
+  output$indCrecimientoBenefNac <- renderInfoBox({
+    indCrecBeneficiariosNac <- mean(BDnacional$'Crecimiento beneficiarios cubiertos')
+    aproximacionCrecBeneficiariosNac <- round(indCrecBeneficiariosNac, 2)
+    porcentajeCrecBeneficiariosNac <- str_c( aproximacionCrecBeneficiariosNac, "%")
+    infoBox(
+      "Crecimiento beneficiarios cubiertos", porcentajeCrecBeneficiariosNac, icon = icon("fas fa-chart-line"),
+      width = 6, color = "orange", fill = TRUE
+    )
+  })
+  
+  ##con los siguientes 3 output$... se envian al ui.R los gráficos de barra.
+  
+  output$ejecutorPublicoVsPrivado <- renderPlot({
     tablaEjecutor <- table(BDiniciativas0$`Tipo Ejecutor`)
     tablaEjecutor <- prop.table(tablaEjecutor)
-    output$ejecutorPublicoVsPrivado <- renderPlot({
-        barplot(tablaEjecutor,
-                xlab = "Tipo de ejecutor", ylab = "Proporción", ylim = c(0, 0.6), 
-                col = c("royalblue", "lightblue"))
-    })
-    
+    barplot(tablaEjecutor,
+            xlab = "Tipo de ejecutor", ylab = "Proporción", ylim = c(0, 0.6), 
+            col = c("royalblue", "lightblue"))
+  })
+  output$iniciativasPorSector <- renderPlot({
     tablaSector <- table(BDiniciativas0$Sector)
     tablaSector <- prop.table(tablaSector)
-    output$iniciativasPorSector <- renderPlot({
-        barplot(tablaSector,
-                xlab = "", ylab = "Proporción", ylim = c(0, 0.3),
-                col = "darkslategray3", las =2, cex.names = 0.7)
-    })
-    
+    barplot(tablaSector,
+            xlab = "", ylab = "Proporción", ylim = c(0, 0.3),
+            col = "darkslategray3", las =2, cex.names = 0.7)
+  })
+  output$pPotencial <- renderPlot({
     Porcentaje<- (BDregiones$PP16 / BDregiones$Población_Total)*100
-    output$pPotencial <- renderPlot({
-        
-        ggplot(data=BDregiones, aes(x= Región, y= Porcentaje, fill= Región)) + 
-            geom_bar(stat="identity", position="stack") +
-            scale_x_discrete("Regiones") +
-            scale_fill_manual(values=c("#CC0000","#FF6633","#CC9900","#996633","#FFFF33","#009900","#66FF66","#336666","#009999","#0033FF","3366FF","#000099","#FF66FF","#660099","#990066"))
-        
-    })
+    ggplot(data=BDregiones, aes(x= Región, y= Porcentaje, fill= Región)) + 
+      geom_bar(stat="identity", position="stack") +
+      scale_x_discrete("Regiones") +
+      scale_fill_manual(values=c("#CC0000","#FF6633","#CC9900","#996633","#FFFF33","#009900","#66FF66","#336666","#009999","#0033FF","3366FF","#000099","#FF66FF","#660099","#990066"))
     
-##se envia al ui.R la tabla interactiva de iniciativas con el nombre de iniciativax
-    output$iniciativax <- renderDataTable(BDiniciativas0)
-    
-##cajitas de indicadores para la seccion del sidebar de indicadores por iniciativa.
-    output$indTiempoTranscurrido <- renderInfoBox({
-        infoBox(
-            "Tiempo presupuestado transcurrido", "55%", icon = icon("fas fa-calendar-alt"),
-            width = 4, color = "orange", fill = TRUE
-        )
-    })
-
-    output$indAvanceActividades <- renderInfoBox({
-        infoBox(
-            "Avance según actividades", "60%", icon = icon("fas fa-clipboard"),
-            width = 4, color = "green", fill = TRUE
-        )
-    })
-    output$indAvanceComponentes <- renderInfoBox({
-        infoBox(
-            "Avance según componentes", "63%", icon = icon("fas fa-clipboard-list"),
-            width = 4, color = "blue", fill = TRUE
-        )
-    })
-    
-    
-    #intento de indicadorbeneficiarios
-    output$indBeneficiarios <- infoBox({
-      
-      benefObj <- filter(BDseguimiento, BDseguimiento$`Nombre Proyecto` == input$Iniciativas)
-      benefObj <- select(benefObj, "Beneficiarios objetivos")
-      View(benefObj)
-      
-      benefEfe <- filter(BDseguimiento, BDseguimiento$`Nombre Proyecto` == input$Iniciativas)
-      benefEfe <- select(benefEfe, "Beneficiarios efectivos")
-      View(benefEfe)
-      
-      indBeneficiarios <- ((benefEfe/benefObj)*100) 
-      
-      infoBox(indBeneficiarios)
-    })
+  })
   
-    output$indBeneficiariosEmpresa <- renderInfoBox({
-        infoBox(
-            "Beneficiarios empresa cubiertos", "14%", icon = icon("fas fa-industry"),
-            width = 6, color = "purple", fill = TRUE
-        )
-    })
+  ##se envia al ui.R la tabla interactiva de iniciativas con el nombre de iniciativax
+  output$tablaIniciativa <- renderDataTable(BDiniciativas0)
+  
+  
+  output$selecAño <- renderUI({
     
-##se crea la tabla interactiva para la seccion de indicadores por iniciativa.
-    BDiniciativas1 <- read_sheet("https://docs.google.com/spreadsheets/d/1QkMjIkeZgyCdhZYTHwZai9BsjN2lamvf_8AgwSRS5XI/edit#gid=0%22", sheet = "actividades y componentes")
-    tablaIniciativa1 <- BDiniciativas1[, 3:11]
-    output$iniciativa1 <- renderDataTable(tablaIniciativa1)
+    selectInput(inputId = "Iniciativas", label = "Iniciativas:", 
+                choices = unique(BDiniciativas0[BDiniciativas0$`Año`== input$Año, "Nombre Proyecto"])
+                
+    )
+  })
+  
+  ##cajitas de indicadores para la seccion del sidebar de indicadores por iniciativa.
+  output$indTiempoTranscurrido <- renderInfoBox({
+    infoBox(
+      "Tiempo presupuestado transcurrido", "55%", icon = icon("fas fa-calendar-alt"),
+      width = 4, color = "orange", fill = TRUE
+    )
+  })
+  output$indAvanceActividades <- renderInfoBox({
+    ##Fórmula de indicador avance actividades
+    #Obtengo los datos de las iniciativas
+    x <- input$Iniciativas
+    y <- input$Año
+    activ <- filter(BDactividades, str_detect (BDactividades$`Nombre Proyecto`, x))
+    activ <- filter (activ, str_detect (activ$`Año`, y) == TRUE)
+    #elimino duplicados en caso de que existan
+    activ <- activ[!duplicated(activ),]
+    ##para actividades realizadas
+    ##selecciono el estado de las actividades
+    estadoAct <- select(activ, "Estado act")
+    numActTotales <- nrow(estadoAct)
+    actRealizadas <- filter(estadoAct, str_detect(activ$`Estado act`, "entregado") == TRUE)
+    numActRealizadas <- nrow(actRealizadas)
+    indAvanceAct <- ((numActRealizadas/numActTotales)*100)
+    indAvanceAct <- round(indAvanceAct, 2)
+    indAvanceAct <- str_c(indAvanceAct, "%")
+    infoBox(
+      "Avance según actividades", indAvanceAct, icon = icon("fas fa-clipboard"),
+      width = 4, color = "green", fill = TRUE
+    )
+  })
+  output$indAvanceComponentes <- renderInfoBox({
+    ##Fórmula de indicador comp actividades
+    #Obtengo los datos de las iniciativas
+    x <- input$Iniciativas
+    y <- input$Año
+    componentes <- filter(BDactividades, str_detect (BDactividades$`Nombre Proyecto`, x) == TRUE)
+    coponentes <- filter (componentes, str_detect (componentes$`Año`, y) == TRUE)
     
+    #elimino duplicados en caso de que existan
+    componentes <- componentes[!duplicated(componentes),]
     
+    #para componentes realizados
+    #selecciono el estado de los componentes
+    estadoComp <- select(componentes, "Componente", "Estado comp")
+    estadoComp2 <- distinct(estadoComp)
+    estadoComp3 <- select(estadoComp2, "Estado comp")
+    numCompTotales <- nrow(estadoComp2)
     
+    compRealizados <- filter(estadoComp3, str_detect(estadoComp3$`Estado comp` , "entregado") == TRUE)
+    numCompRealizados <- nrow(compRealizados)
+    
+    indAvanceComp <- ((numCompRealizados/numCompTotales)*100)
+    indAvanceComp2 <- round(indAvanceComp, 2)
+    indAvanceComp3 <- str_c(indAvanceComp2, "%")
+    
+    infoBox(
+      "Avance según componentes", indAvanceComp3, icon = icon("fas fa-clipboard-list"),
+      width = 4, color = "blue", fill = TRUE
+    )
+  })
+  output$indBeneficiarios <- renderInfoBox({
+    x <- input$Iniciativas
+    y <- input$Año
+    
+    benefIniciativas <- filter (BDseguimiento, str_detect (BDseguimiento$`Nombre Proyecto`, x) == TRUE)
+    benefIniciativas <- filter (benefIniciativas, str_detect (benefIniciativas$`Año`, y) == TRUE)
+    
+    benefObj <- select(benefIniciativas, "beneficiariosObjetivos")
+    benefEfe <- select(benefIniciativas, "beneficiariosEfectivos")
+    
+    indBeneficiarios <- ((benefEfe/benefObj)*100)
+    indBeneficiarios <- round(indBeneficiarios,2)
+    indBeneficiarios <- str_c(indBeneficiarios, "%")
+    
+    infoBox("Beneficiarios efectivos cubiertos",indBeneficiarios, icon = icon("fas fa-clipboard"),
+            width = 4, color = "green", fill = TRUE
+    )
+  })
+  output$indBeneficiariosEmpresa <- renderInfoBox({
+    infoBox(
+      "Beneficiarios empresa cubiertos", "14%", icon = icon("fas fa-industry"),
+      width = 6, color = "purple", fill = TRUE
+    )
+  })
+  
+  ##se crea la tabla interactiva para la seccion de indicadores por iniciativa.
+  tablaIniciativa1 <- BDactividades[, 3:11]
+  output$iniciativa1 <- renderDataTable(tablaIniciativa1)
+  
 })
+
--- ui.R
+++ ui.R
@@ -1,26 +1,15 @@
-##Se cargan las librerias para utilizar sus funciones
-library(shiny)
-library(shinydashboard)
-library(ggplot2)
-library(plotly)
-library(readxl)
-library(googledrive)
-library(googlesheets4)
-library(tidyverse)
-
-
 ##ShinyUI muestra la parte visual de la pagina, y dashboardPage presenta el dashboard
 shinyUI(dashboardPage(
-
+    
     ##dashboardHeader muestra el encabezado del dashboard
     dashboardHeader(title = "Seguimiento técnico FIC-R"),
     
     ##dashboardSidebar muestra el menu lateral de la pagina
     dashboardSidebar(
         sidebarMenu(id = 'sidebarmenu',
-        menuItem("Estadisticas Región", tabName = "inicio"),
-            menuSubItem("Indicadores por iniciativa", tabName = "iniciativas")
-
+                    menuItem("Estadisticas Región", tabName = "inicio"),
+                    menuSubItem("Indicadores por iniciativa", tabName = "iniciativas")
+                    
         )
     ),
     
@@ -28,70 +17,71 @@ shinyUI(dashboardPage(
     dashboardBody(
         ## tabItems permite ver cada pagina del menu lateral
         tabItems(
-        
+            
             tabItem(tabName = "inicio",
-        
-            h2("Seguimiento iniciativas FIC-R Region"),
-            fluidRow(
-                box(h2("Regional")),
-                box(h2("Nacional")),
-            ),
-            
-            fluidRow(
-                infoBoxOutput("indIniciativasAtrasadasReg", width = 6),
-                infoBoxOutput("indIniciativasAtrasadasNac", width = 6),
-                infoBoxOutput("indSectorPriorizadoReg", width = 6),
-                infoBoxOutput("indSectorPriorizadoNac", width = 6),
-                infoBoxOutput("indCrecimientoSPReg", width = 6),
-                infoBoxOutput("indCrecimientoSPNac", width = 6),
-                infoBoxOutput("indBeneficiariosReg", width = 6),
-                infoBoxOutput("indBeneficiariosNac", width = 6),
-                infoBoxOutput("indCrecimientoBenefReg", width = 6),
-                infoBoxOutput("indCrecimientoBenefNac", width = 6)
-            ),
-            
-            fluidRow(
-                h2("Graficos"),
-            ),
-        
-            fluidRow(
-                box(title = "Ejecutor público vs privado", status = "primary", solidHeader = TRUE,
-                    plotOutput("ejecutorPublicoVsPrivado")),
-                box(title = "Iniciativas por sector", status = "primary", solidHeader = TRUE,
-                    plotOutput("iniciativasPorSector"))
-            ),
-        
-            "\n",
-            fluidRow(
-                box(title= "Población Potencial respecto del total de habitantes por Región", status= "primary", solidHeader = TRUE,width = 12,
-                    plotOutput("pPotencial"))
-            
-            ),
-        
-            fluidRow(
-                h2("Todas las iniciativas"),
-            ),
-        
-            fluidRow(
-                dataTableOutput("iniciativax")
-            )
+                    
+                    h2("Seguimiento iniciativas FIC-R Región"),
+                    fluidRow(
+                        box(h2("Regional")),
+                        box(h2("Nacional")),
+                    ),
+                    
+                    fluidRow(
+                        infoBoxOutput("indIniciativasAtrasadasReg", width = 6),
+                        infoBoxOutput("indIniciativasAtrasadasNac", width = 6),
+                        infoBoxOutput("indSectorPriorizadoReg", width = 6),
+                        infoBoxOutput("indSectorPriorizadoNac", width = 6),
+                        infoBoxOutput("indCrecimientoSPReg", width = 6),
+                        infoBoxOutput("indCrecimientoSPNac", width = 6),
+                        infoBoxOutput("indBeneficiariosReg", width = 6),
+                        infoBoxOutput("indBeneficiariosNac", width = 6),
+                        infoBoxOutput("indCrecimientoBenefReg", width = 6),
+                        infoBoxOutput("indCrecimientoBenefNac", width = 6)
+                    ),
+                    
+                    fluidRow(
+                        h2("Graficos"),
+                    ),
+                    
+                    fluidRow(
+                        box(title = "Ejecutor público vs privado", status = "primary", solidHeader = TRUE,
+                            plotOutput("ejecutorPublicoVsPrivado")),
+                        box(title = "Iniciativas por sector", status = "primary", solidHeader = TRUE,
+                            plotOutput("iniciativasPorSector"))
+                    ),
+                    
+                    "\n",
+                    fluidRow(
+                        box(title= "Población Potencial respecto del total de habitantes por Región", status= "primary", solidHeader = TRUE,width = 12,
+                            plotOutput("pPotencial"))
+                        
+                    ),
+                    
+                    fluidRow(
+                        h2("Todas las iniciativas"),
+                    ),
+                    
+                    fluidRow(
+                        dataTableOutput("tablaIniciativa")
+                    )
             ),
             
             tabItem(tabName = "iniciativas",
                     
                     h2("Indicadores por iniciativa"),
                     fluidRow(
-                        box(selectInput(inputId = "Iniciativas", label = "Iniciativas:", 
-<<<<<<< Updated upstream
-                                        choices = unique("lista nombres iniciativas")
-=======
-                                        choices = unique(BDiniciativas0$`Nombre Proyecto`),
-                                        selected= BDiniciativas0$`Nombre Proyecto`[1]
->>>>>>> Stashed changes
-                                        )
-                            )
-                        
-                        
+                        box(
+                            #agrego el data input
+                            selectInput(
+                                inputId =  "Año", 
+                                label = "Seleccione el año de las iniciativas:", 
+                                choices = 2016:as.numeric(format(Sys.Date(),"%Y"))
+                                
+                            ),
+                            uiOutput("selecAño")
+                            
+                            
+                        )
                     ),
                     
                     fluidRow(
@@ -106,9 +96,8 @@ shinyUI(dashboardPage(
                         dataTableOutput("iniciativa1")
                     )
                     
-                    
             )
-          
+            
         )
     )
 ))
